228d44c19848c9b6028f7555be38b5f3db267c36
Ferhat Ziba <fero.ziba@gmail.com>
2013-06-01 11:52:14 +0200

    intial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bizcademy::Application.config.secret_token = '6cd1b0ec32710f1aa1b1cc138a02359fc9f0193b4c450f57ab851d6a94bd272f19c83d37df8426c08c655eeca460cd160e759b98a29323d6d37ea259e0d913f5'
